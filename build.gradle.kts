edbd78087b4675491bed84dd6032ca4da84c91f2
miri <mirmanasreh@aau.at>
2026-03-12 12:46:52 +0100

    Step 1: fixed Leaderboard sorting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+
+
 plugins {
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.serialization") version "2.3.10"
-    kotlin("plugin.spring") version "2.3.10"
-    id("org.springframework.boot") version "4.0.1"
-    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.serialization") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "at.aau.serg"
