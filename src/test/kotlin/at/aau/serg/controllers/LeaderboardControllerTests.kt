06e8d5b693e92ea8a0a580b7c33e3d92ce4da17c
miri <mirmanasreh@aau.at>
2026-03-14 10:32:51 +0100

    Added invalidRankLarge test in LeaderboardTest

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -75,8 +75,13 @@ class LeaderboardControllerTests {
         assertEquals(400,res.statusCode.value())
     }
 
+    @Test
+    fun test_getLeaderboard_invalidRankLarge_returns400() {
+        whenever(mockedService.getGameResults()).thenReturn(listOf(GameResult(1, "first", 20, 10.0)))
 
-
+        val res = controller.getLeaderboard(200)
+        assertEquals(400,res.statusCode.value())
+    }
 
 
 
