666da03eb05f2ff04631dd03cbc63a4febc627f4
miri <mirmanasreh@aau.at>
2026-03-14 10:54:43 +0100

    Added returnValidResult test

--- src/test/kotlin/at/aau/serg/controllers/GameResultControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/GameResultControllerTests.kt
@@ -1,8 +1,14 @@
 package at.aau.serg.controllers
 
+import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when` as whenever
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
 
 class GameResultControllerTests {
 
@@ -15,7 +21,14 @@ class GameResultControllerTests {
         controller = GameResultController(mockedService)
     }
 
+    @Test
+    fun test_getGameResult_returnsValidResult() {
+        val first = GameResult(1, "first", 20, 20.0)
+        whenever(mockedService.getGameResult(1)).thenReturn(first)
 
+        val res = controller.getGameResult(1)
+        assertEquals(first, res)
+    }
 
 
 }
