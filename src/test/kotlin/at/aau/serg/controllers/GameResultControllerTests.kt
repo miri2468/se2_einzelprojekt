0cbc24330e3f692773cb7548447b4516b555cd63
miri <mirmanasreh@aau.at>
2026-03-14 10:56:42 +0100

    Added returnsNull test

--- src/test/kotlin/at/aau/serg/controllers/GameResultControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/GameResultControllerTests.kt
@@ -30,5 +30,13 @@ class GameResultControllerTests {
         assertEquals(first, res)
     }
 
+    @Test
+    fun test_getGameResult_returnsNull() {
+        whenever(mockedService.getGameResult(200)).thenReturn(null)
+
+        val res = controller.getGameResult(200)
+        assertNull(res)
+    }
+
 
 }
