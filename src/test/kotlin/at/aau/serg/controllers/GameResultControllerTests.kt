cc4ed7ae80391d943f083bec7ccd4e4e5233454f
miri <mirmanasreh@aau.at>
2026-03-14 10:52:38 +0100

    Created new GameResultControllerTest file

--- src/test/kotlin/at/aau/serg/controllers/GameResultControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/GameResultControllerTests.kt
@@ -1,16 +1,10 @@
 package at.aau.serg.controllers
 
-import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNull
-import org.mockito.Mockito.`when` as whenever // when is a reserved keyword in Kotlin
 
-class GameResultControllerTest {
+class GameResultControllerTests {
 
     private lateinit var mockedService: GameResultService
     private lateinit var controller: GameResultController
